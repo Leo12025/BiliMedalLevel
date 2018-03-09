316cd3561c34e5d333b63f9aa303acdd6310e77a
dust0517 <36874506+dust0517@users.noreply.github.com>
2018-03-09 09:29:28 +0800

    首版

---
+++ 2.php
@@ -0,0 +1,12 @@
+﻿<?php
+header("Content-type:text/html;charset=utf-8");
+
+  echo '<link rel="stylesheet" href="medal.css"/>';
+  for ($x=1; $x<=20; $x++) {
+echo '<div class="fans-medal-item level-'.$x.'">';
+echo '<span class="label">萌动</span>';
+echo '<span class="level">'.$x.'</span></div> ';
+if($x%4==0){echo '<br/>';}
+} 
+
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+﻿<?php
+header("Content-type:text/html;charset=utf-8");
+$level1=getmedal(21);
+echo '<link rel="stylesheet" href="medal.css"/>';
+echo '<div class="fans-medal-item level-'.$level1['level'].'">';
+echo '<span class="label">'.$level1['name'].'</span>';
+echo '<span class="level">'.$level1['level'].'</span></div>';
+   
+function getmedal($fans){
+	/*
+	 * 首先不推荐引用b站的数据，因为拉取数据需要二次判断，此处可以选择新增自定义字段获取数据
+	 * 
+	 * 默认的则遵循此处name
+	 */
+        $level = [
+            0 => ['max' => 1000, 'min' => 0, 'name' => '新手', 'level' => 1],
+            1 => ['max' => 10000, 'min' => 1000, 'name' => '小将', 'level' => 2],
+            2 => ['max' => 20000, 'min' => 10000, 'name' => '中将', 'level' => 3],
+            3 => ['max' => 50000, 'min' => 20000, 'name' => '上将', 'level' => 4],
+            4 => ['max' => 100000, 'min' => 50000, 'name' => '大将', 'level' => 5],
+            5 => ['max' => 999999999, 'min' => 100000, 'name' => '将军', 'level' => 6],
+        ];
+        foreach ($level as $value) {
+            if (($fans >= $value['min']) && ($fans < $value['max'])) {
+                return $value;
+            }
+        }
+	}
+?>
